1b2de2967c26190ac0aaab5a9836a2fa9d1b23e0
Nick Nisi <nick@nisi.org>
2015-05-22 05:58:04 +0000

    update photobooth to use RGB LED indicators
    * green = ready for user
    * yellow = pose
    * red = doin' stuff

--- photobooth.py
+++ photobooth.py
@@ -10,26 +10,59 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(led_pin, GPIO.OUT);
 GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # falling edge detection
 
+# setup rgb led
+red_pin = 12
+green_pin = 16
+blue_pin = 18
+
+GPIO.setup(red_pin, GPIO.OUT)
+GPIO.setup(green_pin, GPIO.OUT)
+GPIO.setup(blue_pin, GPIO.OUT)
+
+# setup the colors
+freq = 100 # hz
+RED = GPIO.PWM(red_pin, freq)
+RED.start(0)
+GREEN = GPIO.PWM(green_pin, freq)
+GREEN.start(0)
+BLUE = GPIO.PWM(blue_pin, freq)
+BLUE.start(0)
+
+# colors
+red = [60, 0, 0]
+green = [0, 60, 0]
+yellow = [60, 60, 0]
+
+
 def cleanup():
 	print('Closing...')
 	GPIO.cleanup()
 atexit.register(cleanup)
 
-def blink(led, numTimes, speed):
+def setcolor(color):
+	RED.ChangeDutyCycle(color[0])
+	GREEN.ChangeDutyCycle(color[1])
+	BLUE.ChangeDutyCycle(color[2])
+
+def blankcolor():
+	setcolor([0, 0, 0])
+
+def blink(color, numTimes, speed):
 	for i in range(0, numTimes):
-		GPIO.output(led, True)
+		setcolor(color)
 		sleep(speed)
-		GPIO.output(led, False)
+		blankcolor()
 		sleep(speed)
 
-def snap(pressed):
+def snap(colo):
 	pics = 0
 	while pics < 4:
 		print('pose!')
 		sleep(1.5)
-		blink(led_pin, 5, 0.4)
-		blink(led_pin, 5, 0.1)
+		blink(yellow, 5, 0.4)
+		blink(yellow, 5, 0.1)
 		print('SNAP')
+		setcolor(red)
 		gpout = subprocess.check_output('gphoto2 --capture-image-and-download --keep --filename /home/pi/code/pi-photobooth/.photos/photo%H%M%S.jpg', stderr=subprocess.STDOUT, shell=True)
 		print(gpout)
 		if "ERROR" not in gpout:
@@ -39,16 +72,17 @@ def snap(pressed):
 			print(gpout)
 		# sleep(0.5)
 	# TODO: upload LED
+	print('assembling photo strip')
 	assembleout = subprocess.check_output('sudo /home/pi/code/pi-photobooth/assemble_and_upload', shell=True)
 	print(assembleout)
+	setcolor(green)
 
 
-blink(led_pin, 2, 1)
+blink(red, 2, 1)
+setcolor(green)
 
 GPIO.add_event_detect(button_pin, GPIO.FALLING, callback=snap, bouncetime=300)
 
 while True:
-	# input_state = GPIO.input(button_pin)
-	# if input_state == False:
-	#	  print('button pressed')
 	sleep(0.2)
+
