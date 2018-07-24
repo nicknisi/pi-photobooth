3b299a030522176bb813a2c16c39d8e14a49c6c6
George Parris <george@georgeparris.com>
2018-07-23 23:35:38 -0400

    Ella's 1st Birthday Photo Booth

--- photobooth.py
+++ photobooth.py
@@ -6,23 +6,31 @@ import pygame
 import os
 import socket
 import threading
+import PIL
+from PIL import Image
+import shutil
+import cups
 
 PIC_COUNT = 4
 
-# variables
-transform_x = 800
-transform_y = 480
+# Variables:
+#   Screen size:
+#transform_x = 1680
+#transform_y = 1050
+transform_x = 1920
+transform_y = 1080
 offset_x = 0
 offset_y = 0
 
+#    GPIO:
 led_pin = 4
 button_pin = 17
 halt_pin = 4
 network_pin = 23
 real_path = os.path.dirname(os.path.realpath(__file__))
 temp_dir = real_path + '/.photos'
-# photos_dir = real_path + '/photos'
-photos_dir = '/media/usb/photos'
+raw_dir = real_path + '/raws'
+photos_dir = real_path + '/combined_photos'
 
 
 GPIO.setmode(GPIO.BCM)
@@ -112,41 +120,95 @@ def show_image(image_path):
 	screen.blit(img, (offset_x, offset_y))
 	pygame.display.flip()
 
+# Print the photo
+def printPhoto(photo):
+        print('Printing')
+
+        conn = cups.Connection()
+        printers = conn.getPrinters()
+        printer_name = printers.keys()[0]
+        conn.printFile(printer_name, photo,"TITLE",{})
+
+
+def combineImages(photo):
+    #displayStatus('Please wait. Processing Images')
+                            
+        # Do the merging
+        blankImage = Image.open(real_path + '/images/blank.jpg')
+
+        image1 = Image.open(temp_dir + '/image0.jpg')        
+        #image1 = image1.resize((600,400),PIL.Image.ANTIALIAS)
+        #blankImage.paste(image1, (190,190))
+        blankImage.paste(image1, (160,160))
+
+        image2 = Image.open(temp_dir + '/image1.jpg')        
+        #image2 = image2.resize((600,400),PIL.Image.ANTIALIAS)
+        #blankImage.paste(image2, (810,190))
+        blankImage.paste(image2, (840,160))
+
+        image3 = Image.open(temp_dir + '/image2.jpg')        
+        #image3 = image3.resize((600,400),PIL.Image.ANTIALIAS)
+        #blankImage.paste(image3, (190,610))
+        blankImage.paste(image3, (160,630))
+
+        image4 = Image.open(temp_dir + '/image3.jpg')        
+        #image4 = image4.resize((600,400),PIL.Image.ANTIALIAS)
+        #blankImage.paste(image4, (810,610))
+        blankImage.paste(image4, (840,630))
+
+        blankImage.save(photo, quality=100)
+
 def snap():
 	pics = 0
 	now = strftime('%Y-%m-%d-%H:%M:%S')
+        shutil.rmtree(temp_dir)
+        if not os.path.exists(temp_dir):
+                os.makedirs(temp_dir)
 
 	while pics < PIC_COUNT:
 		print('pose!')
-		show_image(real_path + '/images/pose.png')
-		sleep(1)
+		show_image(real_path + '/images/pose.jpg')
+		#sleep(1)
 		blink(yellow, 5, 0.4)
 		blink(yellow, 5, 0.1)
 		print('SNAP')
 		setcolor(red)
-		filename = temp_dir + '/photo-' + now + '-' + `pics` + '.jpg'
+		filename = raw_dir + '/photo-' + now + '-' + `pics` + '.cr2'
+		outFileName = temp_dir + '/image' + `pics` + '.jpg'
 		print('filename: ' + filename)
 		gpout = subprocess.check_output('gphoto2 --capture-image-and-download --keep --filename ' + filename, stderr=subprocess.STDOUT, shell=True)
+                #ufrawCmd = "ufraw-batch --out-type jpg %s --output=%s --size=600,400 --silent" % (filename, outFileName)
+                ufrawCmd = "ufraw-batch --out-type jpg %s --output=%s --size=640,480 --silent" % (filename, outFileName)
+                print ufrawCmd
+                #subprocess.Popen(["ufraw-batch","--out-type","jpg",filename,"--output=outFileName","--size=600,400"])
+                p = subprocess.Popen(ufrawCmd.split())
 		print(gpout)
 		if "ERROR" not in gpout:
 			print('SUCCESS!')
 			show_image(filename)
-			sleep(2)
+			sleep(5)
 			pics += 1
 		else:
-			print(gpout)
+                        print ('ERROR!')
+		        print(gpout)
 			blink([80, 0, 80], 5, 0.5)
 		# sleep(0.5)
 	# TODO: upload LED
 	print('assembling photo strip')
-	show_image(real_path + '/images/waiting.png')
+	show_image(real_path + '/images/waiting.jpg')
+        sleep (5)
 	photo = photos_dir + '/' + now + '.jpg'
-	assembleout = subprocess.check_output('sudo ' + real_path + '/assemble_and_upload ' + photo, shell=True)
-	print(assembleout)
+        #print ('ufraw:')
+        #os.system("ufraw-batch --out-type jpg " + temp_dir + "/*.cr2")
+        p.wait()
+        combineImages(photo)
+	#assembleout = subprocess.check_output('sudo ' + real_path + '/assemble_and_upload ' + photo, shell=True)
+	#print(assembleout)
 	show_image(photo)
-	sleep(3)
+        printPhoto(photo)
+	sleep(10)
 	setcolor(green)
-	show_image(real_path + '/images/ready.png')
+	show_image(real_path + '/images/ready.jpg')
 
 def check_connectivity():
 	# turn on the red light if we're not connected
@@ -155,17 +217,17 @@ def check_connectivity():
 set_interval(check_connectivity, 60)
 
 
-
 # blink(red, 2, 1)
 setcolor(green)
 
 # GPIO.add_event_detect(button_pin, GPIO.FALLING, callback=snap, bouncetime=300)
 
-show_image(real_path + '/images/ready.png')
+show_image(real_path + '/images/ready.jpg')
 
 while True:
 	if (GPIO.input(button_pin) != 1):
 		snap()
 		sleep(1)
 	elif (GPIO.input(halt_pin) != 1):
-		reboot()
+                print("something")
+		#reboot()
